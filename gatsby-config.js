69bd763bef08becf855810b228b3ff48f0f35d3f
bablu <nithinkumarsn21@gmail.com>
2021-01-06 23:31:38 +0530

    changes

--- gatsby-config.js
+++ gatsby-config.js
@@ -35,15 +35,10 @@ module.exports = {
         display: `standalone`,
         icons: [
           {
-            src: `/android-chrome-192x192.png`,
-            sizes: `192x192`,
-            type: `image/png`,
+            src: `/favicon.ico`,
+            type: `image/ico`,
           },
-          {
-            src: `/android-chrome-512x512.png`,
-            sizes: `512x512`,
-            type: `image/png`,
-          },
+          
         ],
       },
     },
